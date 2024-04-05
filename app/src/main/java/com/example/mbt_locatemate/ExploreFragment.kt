de0a4c6aa8ab8d7222e40144adf52b672d86d949
MatthewHilliard <matthewhilliard2003@gmail.com>
2024-04-05 00:06:59 -0400

    Begun work on setting up explore page and post recycler view

--- app/src/main/java/com/example/mbt_locatemate/ExploreFragment.kt
+++ app/src/main/java/com/example/mbt_locatemate/ExploreFragment.kt
@@ -4,15 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.google.android.material.button.MaterialButtonToggleGroup
 
 class ExploreFragment: Fragment() {
+    private lateinit var segmentedButton: MaterialButtonToggleGroup
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_explore, container, false)
+
+        segmentedButton = view.findViewById(R.id.segmentedButton)
+
+        segmentedButton.addOnButtonCheckedListener { segmentedButton, checkedId, isChecked ->
+            when (checkedId) {
+                R.id.friendsButton -> {
+
+                }
+                R.id.exploreButton -> {
+
+                }
+            }
+        }
 
         return view
     }

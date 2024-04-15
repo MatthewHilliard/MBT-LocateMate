0e33e17ab95056f6648b7489cca91fc6ae9d2631
MatthewHilliard <matthewhilliard2003@gmail.com>
2024-04-14 20:47:46 -0400

    Created friend adapter class for recycler view

---
+++ app/src/main/java/com/example/mbt_locatemate/Friend.kt
@@ -0,0 +1,9 @@
+package com.example.mbt_locatemate
+
+import java.util.UUID
+
+data class Friend (
+    val id: UUID,
+    val username:String,
+    val pfpUrl:String
+)
--- app/src/main/java/com/example/mbt_locatemate/FriendListAdapter.kt
+++ app/src/main/java/com/example/mbt_locatemate/FriendListAdapter.kt
@@ -1,4 +1,38 @@
 package com.example.mbt_locatemate
 
-class FriendListAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+
+class FriendListAdapter(private var friends: List<Friend>) : RecyclerView.Adapter<FriendListAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.list_item_friend, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return friends.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(friends[position])
+    }
+
+    fun updateFriends(newFriends: List<Friend>) {
+        friends = newFriends
+        notifyDataSetChanged()
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val friendUser: TextView = itemView.findViewById(R.id.friend_user)
+        private val pfpImage: ImageView = itemView.findViewById(R.id.pfp_friend)
+        fun bind(friend: Friend) {
+            friendUser.text = friend.username
+            Picasso.get().load(friend.pfpUrl).into(pfpImage)
+        }
+    }
 }

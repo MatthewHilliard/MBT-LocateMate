a3d64bf6cfde7c89142b96589be8d474394ce2be
MatthewHilliard <matthewhilliard2003@gmail.com>
2024-04-14 21:18:11 -0400

    Friends fragment now displays friends from database

--- app/src/main/java/com/example/mbt_locatemate/Friend.kt
+++ app/src/main/java/com/example/mbt_locatemate/Friend.kt
@@ -3,7 +3,6 @@ package com.example.mbt_locatemate
 import java.util.UUID
 
 data class Friend (
-    val id: UUID,
     val username:String,
     val pfpUrl:String
 )
--- app/src/main/java/com/example/mbt_locatemate/FriendsFragment.kt
+++ app/src/main/java/com/example/mbt_locatemate/FriendsFragment.kt
@@ -1,19 +1,49 @@
 package com.example.mbt_locatemate
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.firestore
+import java.util.UUID
 
 class FriendsFragment : Fragment() {
+    private lateinit var layoutManager: RecyclerView.LayoutManager
+    private lateinit var adapter: FriendListAdapter
+    private lateinit var friendRecyclerView: RecyclerView
+
+    private lateinit var auth: FirebaseAuth
+    private val db = Firebase.firestore
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_friends, container, false)
+        friendRecyclerView = view.findViewById(R.id.friend_recycler_view)
+        friendRecyclerView.addItemDecoration(
+            DividerItemDecoration(
+                friendRecyclerView.context,
+                DividerItemDecoration.VERTICAL
+            )
+        )
+
+        layoutManager = LinearLayoutManager(requireContext())
+        friendRecyclerView.layoutManager = layoutManager
+
+        auth = Firebase.auth
+
+        adapter = FriendListAdapter(mutableListOf())
+        friendRecyclerView.adapter = adapter
 
         val backButton = view.findViewById<ImageView>(R.id.friendBackButton)
 
@@ -23,6 +53,35 @@ class FriendsFragment : Fragment() {
                 .replace(R.id.fragment_container, exploreFragment).commit()
         }
 
+        loadFriends("")
         return view
+    }
+
+    private fun loadFriends(search: String) {
+        val userId = auth.currentUser?.uid
+        if (userId != null) {
+            if (search.isEmpty()) {
+                db.collection("friends").document(userId)
+                    .collection("friend_usernames")
+                    .get()
+                    .addOnSuccessListener { friendsSnapshot ->
+                        val friendUsernames = friendsSnapshot.documents.map { it.id }
+
+                        db.collection("users")
+                            .whereIn("username", friendUsernames)
+                            .get()
+                            .addOnSuccessListener { documents ->
+                                val friendList = mutableListOf<Friend>()
+                                for (document in documents) {
+                                    val username = document.getString("username") ?: ""
+                                    val pfpUrl = document.getString("pfp_url") ?: ""
+                                    val friend = Friend(username, pfpUrl)
+                                    friendList.add(friend)
+                                }
+                                adapter.updateFriends(friendList)
+                            }
+                    }
+            }
+        }
     }
 }

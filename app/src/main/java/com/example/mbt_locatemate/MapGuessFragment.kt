fea7c2359450eed04b635ed98540f1ad461dd0cc
picoting <lindsayl.6857@gmail.com>
2024-04-17 01:35:32 -0400

    guess fragment should pull up the map now

--- app/src/main/java/com/example/mbt_locatemate/MapGuessFragment.kt
+++ app/src/main/java/com/example/mbt_locatemate/MapGuessFragment.kt
@@ -1,29 +1,39 @@
-package com.example.mbt_locatemate
-
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.example.mbt_locatemate.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
+class MapsFragment : Fragment(), OnMapReadyCallback {
 
-class MapGuessFragment : Fragment() {
+    private lateinit var map: GoogleMap
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_mapguess, container, false)
-
-        val btnBack = view.findViewById<Button>(R.id.btnBack)
-        btnBack.setOnClickListener {
-            // handle the back button press
-            activity?.onBackPressed()
-        }
-
-        return view
+    @Deprecated("Deprecated in Java")
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
     }
 
-    // Existing code for the map setup
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        // example
+        val sydney = LatLng(-34.0, 151.0)
+        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        map.moveCamera(com.google.android.gms.maps.CameraUpdateFactory.newLatLng(sydney))
+
+        map.uiSettings.isZoomControlsEnabled = true
+        map.uiSettings.isScrollGesturesEnabled = true
+        map.uiSettings.isZoomGesturesEnabled = true
+
+        // set click listener
+        map.setOnMapClickListener { latLng ->
+            map.clear()
+            map.addMarker(MarkerOptions().position(latLng).title("Guess Location"))
+        }
+    }
 }

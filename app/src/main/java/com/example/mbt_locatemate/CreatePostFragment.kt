dcc405facb06c31b76000ce061601727f39e56af
bvogler10 <brinjavogler@gmail.com>
2024-04-17 01:52:00 -0400

    implemented logic to get current location after taking a photo, but not currently working

--- app/src/main/java/com/example/mbt_locatemate/CreatePostFragment.kt
+++ app/src/main/java/com/example/mbt_locatemate/CreatePostFragment.kt
@@ -1,18 +1,33 @@
 package com.example.mbt_locatemate
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
-class CreatePostFragment: Fragment() {
+class CreatePostFragment: Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+
+    private lateinit var nMap: GoogleMap
     private lateinit var mapView: MapView
+    private lateinit var lastLocation: Location
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    companion object {
+        private const val LOCATION_REQUEST_CODE = 1
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,13 +36,47 @@ class CreatePostFragment: Fragment() {
         val view = inflater.inflate(R.layout.fragment_post_create, container, false)
         mapView = view.findViewById(R.id.mapView)
         mapView.onCreate(savedInstanceState)
-        //mapView.getMapAsync(this)
+        mapView.getMapAsync(this)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
         return view
     }
-//    fun onMapReady(googleMap: GoogleMap) {
-//        // Add a marker on your location
-//        val myLocation = LatLng(yourLatitude, yourLongitude)
-//        googleMap.addMarker(MarkerOptions().position(myLocation).title("Your Location"))
-//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation))
-//    }
+    override fun onMapReady(googleMap: GoogleMap) {
+        nMap = googleMap
+        nMap.uiSettings.isZoomControlsEnabled = true
+        nMap.setOnMarkerClickListener(this)
+        setUpMap()
+    }
+
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_REQUEST_CODE)
+            return
+        }
+        nMap.isMyLocationEnabled = true
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+            if (location != null) {
+                lastLocation = location
+                val currentLatLong = LatLng(location.latitude, location.longitude)
+                placeMarker(currentLatLong)
+                
+
+            }
+        }
+    }
+
+    private fun placeMarker(latLong: LatLng) {
+        val markerOptions = MarkerOptions().position(latLong)
+        markerOptions.title("$latLong")
+        nMap.addMarker(markerOptions)
+    }
+
+    override fun onMarkerClick(p0: Marker): Boolean = false
+
 }

b0657023e148bcc63b640a14050fb92141f15d94
bvogler10 <brinjavogler@gmail.com>
2024-04-17 23:21:40 -0400

    beginning some of the logic to add post to database

--- app/src/main/java/com/example/mbt_locatemate/CreatePostFragment.kt
+++ app/src/main/java/com/example/mbt_locatemate/CreatePostFragment.kt
@@ -11,7 +11,7 @@ import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -21,24 +21,26 @@ import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.button.MaterialButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import java.util.UUID
 
 class CreatePostFragment: Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private lateinit var nMap: GoogleMap
-    //private lateinit var mapView: MapView
     private lateinit var lastLocation: Location
     private lateinit var image: ImageView
     var imageTaken = false
+    private lateinit var caption: EditText
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     val resultContract = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
@@ -58,6 +60,7 @@ class CreatePostFragment: Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClic
         val view = inflater.inflate(R.layout.fragment_post_create, container, false)
         //open camera to take a photo
         image = view.findViewById(R.id.imageView)
+        caption = view.findViewById(R.id.captionText)
         if (!imageTaken) {
             GlobalScope.launch(Dispatchers.IO) {
                 val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
@@ -69,6 +72,10 @@ class CreatePostFragment: Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClic
         cancel.setOnClickListener {
             imageTaken = false
         }
+        val post = view.findViewById<MaterialButton>(R.id.postButton)
+        post.setOnClickListener {
+            savePost()
+        }
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(this)
         mapFragment?.onCreate(savedInstanceState)
@@ -76,6 +83,33 @@ class CreatePostFragment: Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClic
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
         return view
     }
+
+    private fun savePost() {
+        val firebaseAuth = FirebaseAuth.getInstance()
+        val currentUser = firebaseAuth.currentUser
+
+        val captionText = caption.text.toString()
+        val imageUrl = "https://firebasestorage.googleapis.com/v0/b/locatemate-cf72b.appspot.com/o/matthew_hilliard_test.jpg?alt=media&token=230d74c2-fba6-41f4-a147-2175029f6e59"
+        val profilePictureUrl = "https://firebasestorage.googleapis.com/v0/b/locatemate-cf72b.appspot.com/o/images%2Fmattpfp.PNG?alt=media&token=31c60078-ec95-4277-8b5a-8046d6cd2341"
+        var username = ""
+        if (currentUser != null) {
+            username = currentUser.displayName.toString()
+        }
+        val location = LatLng(lastLocation.latitude, lastLocation.longitude)
+
+        val post = Post(id= UUID.randomUUID(), username = username, caption = captionText, imgUrl = imageUrl, pfpUrl = profilePictureUrl, location = location)
+        // Upload post to Firestore
+        val db = FirebaseFirestore.getInstance()
+        db.collection("posts")
+            .add(post)
+            .addOnSuccessListener { documentReference ->
+                // Post uploaded successfully
+            }
+            .addOnFailureListener { e ->
+                // Handle failure
+            }
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         nMap = googleMap
         nMap.uiSettings.isZoomControlsEnabled = true
--- app/src/main/java/com/example/mbt_locatemate/Post.kt
+++ app/src/main/java/com/example/mbt_locatemate/Post.kt
@@ -1,6 +1,8 @@
 package com.example.mbt_locatemate
 
+import android.location.Location
 import android.media.Image
+import com.google.android.gms.maps.model.LatLng
 import java.util.UUID
 
 data class Post (
@@ -8,5 +10,6 @@ data class Post (
     val username:String,
     val caption:String,
     val imgUrl:String,
-    val pfpUrl:String
+    val pfpUrl:String,
+    val location: LatLng
 )

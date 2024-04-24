97b365fa543e1f8acf8920cd3fe09ed1d9d0cfa3
MatthewHilliard <matthewhilliard2003@gmail.com>
2024-04-24 11:01:33 -0400

    Further implementing comments recycler view

--- app/src/main/java/com/example/mbt_locatemate/CommentsFragment.kt
+++ app/src/main/java/com/example/mbt_locatemate/CommentsFragment.kt
@@ -5,16 +5,49 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.firestore
 
 class CommentsFragment : BottomSheetDialogFragment() {
+    private lateinit var layoutManager: RecyclerView.LayoutManager
+    private lateinit var adapter: CommentListAdapter
+    private lateinit var commentRecyclerView: RecyclerView
+
+    private lateinit var auth: FirebaseAuth
+    private val db = Firebase.firestore
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_comments_sheet, container, false)
+        commentRecyclerView = view.findViewById(R.id.comment_recycler_view)
+        commentRecyclerView.addItemDecoration(
+            DividerItemDecoration(
+                commentRecyclerView.context,
+                DividerItemDecoration.VERTICAL
+            )
+        )
 
+        layoutManager = LinearLayoutManager(requireContext())
+        commentRecyclerView.layoutManager = layoutManager
+
+        auth = Firebase.auth
+
+        adapter = CommentListAdapter(mutableListOf())
+        commentRecyclerView.adapter = adapter
+
+        loadComments()
         return view
+    }
+
+    private fun loadComments() {
+
     }
 }
